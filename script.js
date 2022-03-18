036d0dcf414058bb811909e21cb494868be6353b
AJM96 <moore96adam@gmail.com>
2022-03-18 14:55:09 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
+{   
+    var rate= document.getElementById('rate').value;
+    var years= document.getElementById('years').value;
+    var principal= document.getElementById('principal').value;
+    var interest= principal * years * rate /100;
+    var dateNow = new Date().getFullYear()+parseInt(years);
+
+    if (principal == undefined || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    else{
+    var resultDisplay=document.getElementById("result");
+
+    resultDisplay.innerHTML= "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + dateNow + "</span>";
+
+    }
+    function SliderValue()
 {
-    p = document.getElementById("principal").value;
-    
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function SliderValue() 
+    {
+        output.innerHTML = this.value;
+    }  
 }
-        
+}
